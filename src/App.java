d8376052f4ee85316bd8a5a654289928a6734411
Vivarsh <63698543+Vivarsh@users.noreply.github.com>
2021-05-27 02:06:41 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class App {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",
-				"/home/ubuntu/chromedriver");
+				"/home/edureka/chromedriver");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--headless");
 		WebDriver driver = new ChromeDriver(chromeOptions);
